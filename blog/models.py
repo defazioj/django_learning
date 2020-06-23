ffb3baa0bebc688e0b985b63393d126ca1359889
Jeffrey De Fazio <jeffrey@Jeffreys-MacBook-Pro.local>
2020-06-23 10:32:59 -0400

    post model and admin functionality

--- blog/models.py
+++ blog/models.py
@@ -7,15 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    # we're not putting parenthesis after timezone.now b/c we don't
-    # want to execute the function at that point,
-    # we just want to pass in the actual function as the default value
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # 2nd arg above just tells django that we want users' posts to be
-    # deleted as well if their profile is deleted
-    # corey also mentions that this is a way way street and that deleting a post
-    # does not delete the user
 
-
-# once we save this file and run makemigrations, we can see this
-# Class reflected in the migrations/0001_initial_.py file
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,12 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'JeffreyD',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'June 14, 2020'
-    },
-    {
-        'author': 'JaneDoe',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'June 15, 2020'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
-    # return HttpResponse('<h1>Blog Home</h1>')
 
 
 def about(request):
